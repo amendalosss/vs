9d642c2804bf70c48ec527fb69b415a7f80b23cd
Yash Soni <yash.soni.in11@gmail.com>
2022-02-19 16:19:30 +0530

    first commit

---
+++ c.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+// #include<iostream.h>
+// #include<conio.h>
+int main()
+{
+    printf("Hello");
+    return 0;  
+}
---
+++ jcc.java
@@ -0,0 +1,6 @@
+class jcc
+{
+    public static void main(String[] args) {
+        System.out.println("hii");
+    }
+}
